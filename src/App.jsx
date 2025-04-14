98864a54f22edb886cbb85fbdcc53a6f49466396
Lyla Lynn <lyladlynn@gmail.com>
2025-04-14 18:23:11 -0500

    finished first tab of workshop

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
+import './index.css'
 
 function App() {
   
 
   return (
     <>
-      
+      <div id="container">
+        <div id="navbar">{/* navigation here */}</div>
+        <div id="main-section">
+          <Routes>
+            <Route path='/blue' element={<h1>Blue</h1>} />
+            <Route path='/red' element={<h1>Red</h1>} />
+          </Routes>
+        </div>
+      </div>
     </>
   )
 }
